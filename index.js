5888ef85813c7f1ff4e850f59cf0614ca4b81348
wujin <jin.wu@dxc.com>
2020-06-30 18:15:26 +0800

    commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./i18n');
+var _i18n;
+
+function init(opt) {
+    _i18n = require("./i18n")(opt);
+}
+
+function getMessage(msgid, ...argv) {
+    return _i18n.__.apply(_i18n, [msgid].concat(argv));
+}
+
+module.exports = {
+    init: init,
+    getMessage: getMessage
+}
